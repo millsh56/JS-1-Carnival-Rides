a9f9739e3ec9512fec9a2131489edca219cfb734
Hannah Mills <millsh56@gmail.com>
2023-07-01 20:52:11 -0400

    finished practice

--- index.js
+++ index.js
@@ -10,30 +10,60 @@ let waterIsOn = true
 //Everything below is fair game for you to power on the rides
 
 //Ride 1 works when:
-    // green wire is connected
+// green wire is connected
 ride1PowerOn = false
 
+if (greenWireConnected) {
+    ride1PowerOn = true
+}
+
+
 //Ride 2 works when 
-    //green and yellow wires are connected
+//green and yellow wires are connected
 ride2PowerOn = false
-
+if (greenWireConnected && yellowWireConnected) {
+    ride2PowerOn = true
+}
 //Ride 3 works when:
-    // green and red wires are connected
-    // water is on 
-    // yellow wire is not connected
+// green and red wires are connected
+// water is on 
+// yellow wire is not connected
 ride3PowerOn = false
-
+if (greenWireConnected && redWireConnected && waterIsOn && !yellowWireConnected) {
+    ride2PowerOn = true
+}
 //Ride 4 has power when:
-    //ride wire is connected
+//ride wire is connected
 ride4PowerOn = false
 //Ride 4 is a slot machine
-    //if a 1 is rolled it should return "star"
-    //if a 2 is rolled it should return "car"
-    //if a 3 is rolled it should return "pot of gold"
-    //if a 4 is rolled it should return "clover"
-    //if a 5 is rolled it should return "bowling ball"
-    //if a 6 is rolled it should return "tiger"
-    //if a 7 is rolled it should return "seven"
+//if a 1 is rolled it should return "star"
+//if a 2 is rolled it should return "car"
+//if a 3 is rolled it should return "pot of gold"
+//if a 4 is rolled it should return "clover"
+//if a 5 is rolled it should return "bowling ball"
+//if a 6 is rolled it should return "tiger"
+//if a 7 is rolled it should return "seven"
 function ride4SlotMachine(rolledNumber){
-    return "nothing"
-}
+switch (rolledNumber) {
+    case 1:
+        console.log("star")
+        break;
+    case 2:
+        console.log("car")
+        break;
+    case 3:
+        console.log("pot of gold")
+        break;
+    case 4:
+        console.log("clover")
+        break;
+    case 5:
+        console.log("bowling ball")
+        break;
+    case 6:
+        console.log("tiger")
+        break;
+    case 7:
+        console.log("seven")
+        break;
+} }
